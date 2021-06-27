c15c9352465cebd2dae7a2222d0e642857d542d5
Seoyoung Joung <seoyoungjoung@Seoyoungui-MacBookAir.local>
2021-06-27 19:05:17 +0900

    커스텀 구분자 지원 문제 해결 및 메소드 분리

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,12 +1,24 @@
-exports.stringCalculator = (str) => {
-  if (str.length === 0) return 0;
-  const num = str.split(/[,\\n]/g);
-  
-  let result = 0;
-  for (let i = 0; i < num.length; i++) {
-    if(num[i]) {
-      result += parseInt(num[i]);
-    }
-  }
-  return result;
+exports.stringCalculator = input => {
+  if (input.length === 0) return 0;
+
+  const { regex, str } = getDelimiter(input);
+  const nums = str.replace(regex, ' ');
+  return sum(nums.split(' '));
 };
+
+const sum = nums =>
+  nums.reduce(
+    (acc, cur) => (acc += parseInt(cur || 0)),
+    0)
+
+const getDelimiter = str => {
+  let regex = /[,\\n]/g;
+
+  if(str.slice(0, 2) === '//') {
+    [delimiter, str] = str.replace('//', '').split('\n');
+    delimiter = delimiter.match(/([^(\[|\])]+)/g)[0];
+    regex = new RegExp(delimiter, "g"); 
+  }
+
+  return { regex, str };
+}
